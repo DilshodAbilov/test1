ffe50a834addca6a79150d5ba0a90939448d4d0d
backendprogrammerpro <bahriyevbahodir66@gmail.com>
2025-09-04 09:37:12 +0500

    new1

--- kaxoot/urls.py
+++ kaxoot/urls.py
@@ -20,9 +20,6 @@ from drf_spectacular.views import SpectacularAPIView,SpectacularRedocView,Specta
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
-    path('api/auth/', include('dj_rest_auth.urls')),
-    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api/auth/social/', include('allauth.socialaccount.urls')),
     path('questions/', include('api.urls')),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
